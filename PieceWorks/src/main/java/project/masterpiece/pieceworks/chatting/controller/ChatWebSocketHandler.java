bcb87dd58ed10c610457613d8baf7cfd91215db9
MovieJIN <moviejin90@naver.com>
2022-01-20 17:56:50 +0900

    error수정

--- PieceWorks/src/main/java/project/masterpiece/pieceworks/chatting/controller/ChatWebSocketHandler.java
+++ PieceWorks/src/main/java/project/masterpiece/pieceworks/chatting/controller/ChatWebSocketHandler.java
@@ -1,9 +1,5 @@
 package project.masterpiece.pieceworks.chatting.controller;
 
-
-
-
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
@@ -11,7 +7,6 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.CloseStatus;
